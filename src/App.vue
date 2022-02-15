b6c77c7909dfef1d985b296f62d28b75c2250a1e
Luiz-Soft <lcfrf@ecomp.poli.br>
2022-02-15 17:11:44 -0300

    created project and folders sctructure

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ColorSelectorPanel/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ColorSelectorPanel from './views/color-selector-panel/ColorSelectorPanel.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ColorSelectorPanel
   }
 }
 </script>
---
+++ src/views/color-selector-panel/ColorSelectorPanel.vue
@@ -0,0 +1,44 @@
+<template>
+<div>
+
+<button type="button" > TEXTO DO BOTAO </button>
+<button type="button" > TEXTO DO BOTAO </button>
+<button type="button" > TEXTO DO BOTAO </button>
+
+<div class="conteudo">
+texto do conteudo
+</div>
+
+</div>
+</template>
+
+
+
+<script>
+
+
+export default {
+ name: 'ColorSelectorPanel', 
+
+ data() {
+    return {
+
+       cores:[{cor: 'azul',
+        conteudo: 'conteudo do componente vermelho'},
+        {cor: 'verde',
+        conteudo: 'conteudo do componente vermelho'},
+        {cor: 'vermelho',
+        conteudo: 'conteudo do componente vermelho'},
+        
+        ]
+    };
+  },
+}
+
+</script>
+
+<style scoped>
+.conteudo{
+    background-color: blue;
+}
+</style>
